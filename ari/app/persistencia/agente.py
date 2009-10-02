1b01df80908189e579b64972eaf59e2003c70e37
juanro.1987 <juanro.1987@516645c2-a45c-11de-b98f-4500b42dc123>
2009-10-02 16:58:59 +0000

    Agente version ¿Final?

--- ari/app/persistencia/agente.py
+++ ari/app/persistencia/agente.py
@@ -1,33 +1,53 @@
 # Source: http://forja.rediris.es/snippet/detail.php?type=snippet&id=24
 # License: GNU General Public License
 
-from sqlobject import *
+import MySQLdb
 
 class Agente:
     
     __conn = None
-    __dbuser = "root"
-    __dbpassword = "ari"
-    __host = "localhost"
-    __port = 3306
-    __dbname = "ari"
-    __mURI="mysql://"+__dbuser+":"+__dbpassword+"@"+__host+":"+str(__port)+"/"+__dbname
 
     class Singleton:
         def __init_( self ):
             self.Agente = None
-            __conn = connectionForURI(__mURI)
+            __conn = MySQLdb.connect("localhost", "root", "ari", "ari")
+            # Metodo para almacenar las transacciones en la BBDD de manera permanente
+            __conn.autocommit()
         
     def __init_( self ):
         if Agente.__conn is None:
             Agente.__conn = Agente.Singleton()
         self.__dict__['_EventHandler_instance'] = Agente.__mInstancia
 
+    # Metodos para obtener y cerrar la conexion con la BBDD
     def getDB ( self ):
-        return connectionForURI(__mURI)
-        
-    def __getattr__(self, aAttr):
+        return __conn
+
+    def close ( self ):
+        __conn.close()
+
+    # Metodos privados para la gestion del agente       
+    def __getattr__( self, aAttr ):
         return getattr(self.__mInstancia, aAttr)
  
-    def __setattr__(self, aAttr, aValue):
+    def __setattr__( self, aAttr, aValue ):
         return setattr(self.__mInstancia, aAttr, aValue)
+
+    # Metodos que implementan operacion CRUD (3 comillas en la consulta)
+    def query ( self, sql ):
+        cursor=self.__conn.cursor()
+        cursor.execute(sql)
+        result=cursor.fetchall()
+        cursor.close()
+        return result
+
+    def execute ( self, sql ):
+        cursor=self.__conn.cursor()
+        cursor.execute(sql)
+        cursor.close()
+    
+
+
+
+
+    
