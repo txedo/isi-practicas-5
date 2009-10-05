119c26f4d79136152dc339cdd0c8c8b13bf7e1c6
juanro.1987 <juanro.1987@516645c2-a45c-11de-b98f-4500b42dc123>
2009-10-05 14:56:04 +0000

    Agente con singleton funcionando

--- trunk/ari/app/persistencia/agente.py
+++ trunk/ari/app/persistencia/agente.py
@@ -1,37 +1,26 @@
-# Source: http://forja.rediris.es/snippet/detail.php?type=snippet&id=24
-# License: GNU General Public License
-
 import MySQLdb
 
-class Agente:
-    
-    __conn = None
+class Singleton(object):
 
-    class Singleton:
-        def __init_( self ):
-            self.Agente = None
-            __conn = MySQLdb.connect("localhost", "root", "ari", "ari")
-            # Metodo para almacenar las transacciones en la BBDD de manera permanente
-            __conn.autocommit()
-        
-    def __init_( self ):
-        if Agente.__conn is None:
-            Agente.__conn = Agente.Singleton()
-        self.__dict__['_EventHandler_instance'] = Agente.__mInstancia
+    __instance=None
+    __conn=None
+
+    # Constructor
+    def __new__(cls):
+        # Si no hay ninguna instancia del agente, se inicializa la conexion de la base de datos
+        if not cls.__instance:
+            cls.__instance=object.__new__(cls)
+        cls.__conn = MySQLdb.connect("localhost", "root", "ari", "ari")
+        # Metodo para almacenar las transacciones en la BBDD de manera permanente
+        cls.__conn.autocommit(1)
+        return cls.__instance
 
     # Metodos para obtener y cerrar la conexion con la BBDD
     def getDB ( self ):
-        return __conn
+        return self.__conn
 
     def close ( self ):
-        __conn.close()
-
-    # Metodos privados para la gestion del agente       
-    def __getattr__( self, aAttr ):
-        return getattr(self.__mInstancia, aAttr)
- 
-    def __setattr__( self, aAttr, aValue ):
-        return setattr(self.__mInstancia, aAttr, aValue)
+        self.__conn.close()
 
     # Metodos que implementan operacion CRUD (3 comillas en la consulta)
     def query ( self, sql ):
@@ -45,9 +34,5 @@ class Agente:
         cursor=self.__conn.cursor()
         cursor.execute(sql)
         cursor.close()
-    
 
 
-
-
-    
