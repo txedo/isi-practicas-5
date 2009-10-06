0d7494b7105cb0a3d4f13766327d29eea977e8b9
juanro.1987 <juanro.1987@516645c2-a45c-11de-b98f-4500b42dc123>
2009-10-06 19:25:46 +0000

    Analizador incorporando la stop_list

--- ari/app/dominio/analyzer.py
+++ ari/app/dominio/analyzer.py
@@ -4,35 +4,51 @@ import os
 
 class Analyzer:
 
+    # Inicializamos los atributos de la clase
     def __init__(self):    
         self.posting_file = {}    
         self.dic = {}
         self.docs = {}
     
+    # Metodo para indexar un directorio
     def file_index(self, path):
+        # Se ignoraran signos de puntuacion
         punctuation = "?!.;:[](),\""
+        # Leemos del fichero la stop_list
+        fd_stop_list = open ("../misc/stoplist","r")
+        stop_list = fd_stop_list.read()
+
         if not os.path.isfile(path):
             pass # Tratamiento de enlaces simbolicos
         else:
+            # Abrimos y leemos el documento
             fd = open(path, "r")
             text = fd.read().lower()
+            # Eliminamos los signos de puntuacion y obtenemos la lista de palabras que forman el texto
             result = text.translate(string.maketrans("",""),punctuation)
             word_list = re.split('\s+', result)
+            # Para cada una de esas palabras, si no pertenece a la stop_list, actualizamos su frecuencia
             for word in word_list:
-                try:
-                    self.posting_file[word]+=1
-                except:
-                    self.posting_file[word]=1
+                if word not in stop_list:
                     try:
-                        self.dic[word]+=1
+                        self.posting_file[word]+=1
+                    # Si es la primera vez que aparece la palabra en el documento, se comprueba si esa 
+                    # palabra ya aparecia en otro documento
                     except:
-                        self.dic[word]=1        
+                        self.posting_file[word]=1
+                        try:
+                            self.dic[word]+=1
+                        except:
+                            self.dic[word]=1        
 
 
+    # Metodo para indexar un directorio
     def folder_index(self, path):
+        # Si no es un directorio, se lanza una excepcion
         if not os.path.isdir(path):
             raise Exception
         else:
+            # Se lista el contenido del directorio y se procesan los ficheros
             list_files = os.listdir(path)
             for f in list_files:
                 full_path = path+"/"+f
@@ -40,11 +56,5 @@ class Analyzer:
                     pass # Tratamiento de directorios
                 else:
                     self.file_index(full_path)
-
-
-        
-        
-        
-
 
 
