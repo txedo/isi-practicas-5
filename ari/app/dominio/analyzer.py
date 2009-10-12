be254a57891e27716e11b75f322744bfb6360c73
Txedo <txedo@users.noreply.github.com>
2009-10-12 20:04:29 +0000

    * Creado el repositorio donde se creara la base documental * Modificado el fichero install. IMPORTANTE: Eliminar la base de datos existente y volverla a crear * Se han hecho nuevos metodos del broker. Falta por hacer un par de ellos * Se ha hecho el parser pero no se ha probado * Creado un fichero de variables globales config.py

--- ari/app/dominio/analyzer.py
+++ ari/app/dominio/analyzer.py
@@ -1,46 +1,45 @@
 import re
 import string
 import os
+import shutil
 
 class Analyzer:
 
     # Inicializamos los atributos de la clase
     def __init__(self):    
-        self.posting_file = {}    
-        self.dic = {}
-        self.docs = {}
-    
+        self.stop_list = (open("../misc/stoplist","r")).read()
+
+
     # Metodo para indexar un directorio
     def file_index(self, path):
         # Se ignoraran signos de puntuacion
-        punctuation = "?!.;:[](),\""
-        # Leemos del fichero la stop_list
-        fd_stop_list = open ("../misc/stoplist","r")
-        stop_list = fd_stop_list.read()
+        #punctuation = "?!.;:[](),\""
 
         if not os.path.isfile(path):
-            pass # Tratamiento de enlaces simbolicos
+            pass # Tratamiento de enlaces simbolicos o ruta incorrecta
         else:
-            # Abrimos y leemos el documento
+            # Insertamos el documento en la base de datos
+            file_name = (path.split("/"))[-1]
+            (system_path, last_id) = broker.insert_doc(file_name)
+            # Copiamos el documento al repositorio
+            shutil.copy2(path, system_path)
+            # Abrimos y leemos el documento linea a linea, pasando cada linea por el parser
             fd = open(path, "r")
-            text = fd.read().lower()
-            # Eliminamos los signos de puntuacion y obtenemos la lista de palabras que forman el texto
-            result = text.translate(string.maketrans("",""),punctuation)
-            word_list = re.split('\s+', result)
-            # Para cada una de esas palabras, si no pertenece a la stop_list, actualizamos su frecuencia
-            for word in word_list:
-                if word not in stop_list:
-                    try:
-                        self.posting_file[word]+=1
-                    # Si es la primera vez que aparece la palabra en el documento, se comprueba si esa 
-                    # palabra ya aparecia en otro documento
-                    except:
-                        self.posting_file[word]=1
-                        try:
-                            self.dic[word]+=1
-                        except:
-                            self.dic[word]=1        
-
+            for line in fd.readlines():
+                word_list = parser(line)
+                # Para cada una de esas palabras, si no pertenece a la stop_list, actualizamos su frecuencia
+                for word in word_list:
+                    if broker.exist_term_posting_file(word):
+                        broker.update_term_posting_file(word)
+                    else:
+                        # Si es la primera vez que aparece la palabra en el documento, se comprueba si esa 
+                        # palabra ya aparecia en otro documento
+                        broker.insert_term_posting_file(word, last_id)
+                        if broker.exist_term_dic(word):
+                            broker.update_term_dic(word)
+                        else:
+                            broker.insert_term_dic(word)
+                            
 
     # Metodo para indexar un directorio
     def folder_index(self, path):
@@ -58,3 +57,30 @@ class Analyzer:
                     self.file_index(full_path)
 
 
+    def parser(self, line):
+        result = {}
+        word_list = {}
+        ip_pattern = re.compile('([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})')
+        # Separar las palabras por el separador espacio \s
+        foo = line.translate(string.maketrans("",""),"\t+")
+        word_list = foo.split(" ")
+        # Para cada palabra de la lista, si NO esta en la stop_list, parsearla
+        for word in word_list:
+            if len(word) > 0 and word not in self.stop_list:
+                # Si la palabra no es web, ni email, ni direccion IP, se quitan los signos de puntuacion y se comprueba si alguna de las dos partes NO es palabra
+                if ip_pattern.match(word): # Es una IP y la tratamos como tal
+                    result.append(word)
+                else: # sea web, email o lo que sea, quitamos signos de puntuacion
+                    word_parts = (re.sub('[%s]' % re.escape(string.punctuation), " ", word)).split(" ")
+                    is_compound_word = False
+                    for w in word_parts:
+                        if w in self.stop_list:
+                            is_compound_word = True
+                            break
+                    # Si se puede separar, meto en la BD todas las partes
+                    if not is_compound_word:
+                        result.extend(word_parts)
+                    # Si no se puede separar, meto la palabra compuesta
+                    else:
+                        result.append(word)
+        return result
