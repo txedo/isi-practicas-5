f52dc45763f891742308475a1417ce474912efc6
juanro.1987 <juanro.1987@516645c2-a45c-11de-b98f-4500b42dc123>
2009-10-15 16:05:37 +0000

    Añadidas excepciones

--- trunk/ari/app/dominio/analyzer.py
+++ trunk/ari/app/dominio/analyzer.py
@@ -22,40 +22,48 @@ class Analyzer:
     # Metodo para indexar un fichero
     def file_index(self, path):
         analyzing_directory = True
+        # Es un enlace simbolico o no existe la ruta
         if not os.path.isfile(path):
-            print path
-            pass # Tratamiento de enlaces simbolicos o ruta incorrecta
+            raise Exception, path+" is not a file"
         else:
             # Controlamos si indexamos un directorio o un fichero
             if self.dao == None:
                 self.dao = dao.Dao()
                 analyzing_directory = False
 
-            # Insertamos el documento en la base de datos
             file_name = (path.split("/"))[-1] # Tomamos el nombre del archivo
-            (system_path, last_id) = self.dao.insert_doc(file_name) # Recuperamos la ruta del repositorio y el id de ese documento
+            try:
+                (system_path, last_id) = self.dao.insert_doc(file_name) # Recuperamos la ruta del repositorio y el id de ese documento
+            except:
+                raise
             # Copiamos el documento al repositorio
-            shutil.copy2(path, system_path)
-            # Abrimos y leemos el documento linea a linea, pasando cada linea por el parser
-            fd = open(path, "r")
-            for line in fd.readlines():
-                word_list = self.parser(line.lower())
-                # Si el parser ha procesado alguna palabra, se actualiza la base de datos
-                if len(word_list) > 0:
-                    # Para cada una de las palabras procesadas y devueltas por el parser, si no pertenece a la stop_list, actualizamos su frecuencia
-                    for word in word_list:
-                        # Si la palabra ya aparecia en el documento (es decir, en el posting_file de este documento), se aumenta su frecuencia
-                        if self.dao.exist_term_posting_file(word, last_id):
-                            self.dao.update_term_posting_file(word, last_id)
-                        else:
-                            # Si es la primera vez que aparece la palabra en el documento, se comprueba si esa 
-                            # palabra ya aparecia en otro documento. Si no, se anade al diccionario (ademas de al posting_file)
-                            if self.dao.exist_term_dic(word):
-                                self.dao.update_term_dic(word)
+            try:
+                shutil.copy2(path, system_path)
+            except: 
+                raise
+            try:
+                # Abrimos y leemos el documento linea a linea, pasando cada linea por el parser
+                fd = open(path, "r")
+                for line in fd.readlines():
+                    word_list = self.parser(line.lower())
+                    # Si el parser ha procesado alguna palabra, se actualiza la base de datos
+                    if len(word_list) > 0:
+                        # Para cada una de las palabras procesadas y devueltas por el parser, si no pertenece a la stop_list, actualizamos su frecuencia
+                        for word in word_list:
+                            # Si la palabra ya aparecia en el documento (es decir, en el posting_file de este documento), se aumenta su frecuencia
+                            if self.dao.exist_term_posting_file(word, last_id):
+                                self.dao.update_term_posting_file(word, last_id)
                             else:
-                                self.dao.insert_term_dic(word)
-                            self.dao.insert_term_posting_file(word, last_id)
+                                # Si es la primera vez que aparece la palabra en el documento, se comprueba si esa 
+                                # palabra ya aparecia en otro documento. Si no, se anade al diccionario (ademas de al posting_file)
+                                if self.dao.exist_term_dic(word):
+                                    self.dao.update_term_dic(word)
+                                else:
+                                    self.dao.insert_term_dic(word)
+                                self.dao.insert_term_posting_file(word, last_id)
                         
+            except:
+                raise
             if not analyzing_directory:
                 self.dao.close()
                             
@@ -64,18 +72,21 @@ class Analyzer:
     def folder_index(self, path):
         # Si no es un directorio, se lanza una excepcion
         if not os.path.isdir(path):
-            raise Exception
+            raise Exception, path+" is not a directory"
         else:
-            # Se lista el contenido del directorio y se procesan los ficheros
-            list_files = os.listdir(path)
-            self.dao = dao.Dao()
-            for f in list_files:
-                full_path = path+"/"+f
-                if os.path.isdir(full_path):
-                    pass # Tratamiento de directorios
-                else:
-                    self.file_index(full_path)
-            self.dao.close()
+            try: 
+                # Se lista el contenido del directorio y se procesan los ficheros
+                list_files = os.listdir(path)
+                self.dao = dao.Dao()
+                for f in list_files:
+                    full_path = path+"/"+f
+                    if os.path.isdir(full_path):
+                        pass # Tratamiento de directorios
+                    else:
+                        self.file_index(full_path)
+                self.dao.close()
+            except: 
+                raise
 
 
     def parser(self, line):
