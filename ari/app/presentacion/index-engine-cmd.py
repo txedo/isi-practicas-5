cc4599acf9dd0ef0f3e913d41b202cc01aef6d6d
Txedo <txedo@users.noreply.github.com>
2009-10-20 18:18:13 +0000

    Interfaces de usuario con tiempos y sin excepciones Corregida longitud de los terminos en la base de datos    NOTA: eliminar y crear de nuevo la base de datos Cambiado analyzer para obtener el fichero que se esta indexando

--- ari/app/presentacion/index-engine-cmd.py
+++ ari/app/presentacion/index-engine-cmd.py
@@ -17,6 +17,7 @@ sys.path.append('../dominio')
 import analyzer
 import MySQLdb
 import datetime
+import threading, time
 
 psyco.full()
 
@@ -42,9 +43,18 @@ else:
     if options.file_name:
         try:
             d1 = datetime.datetime.now()
-            analyzer.file_index(options.file_name)
+            t = threading.Thread(target=analyzer.file_index, args=(options.file_name,))
+            t.start()
+            time.sleep(0.2)
+            label = str(analyzer)
+            print label
+            while analyzer.working:
+                new_label = str(analyzer)
+                if label <> new_label:
+                    print new_label
+                time.sleep(0.2)
             d2 = datetime.datetime.now()
-            print d2-d1
+            print "Index finished in ", d2-d1
         except MySQLdb.Error, e:
             print "SQL Exception: "+e.args[1]
         except FileException, e:
@@ -53,7 +63,30 @@ else:
             print "Exception: "+str(e)
     elif options.directory_name:
         try:
-            analyzer.folder_index(options.directory_name)
+            ndoc=1
+            d1 = datetime.datetime.now()
+            t = threading.Thread(target=analyzer.folder_index, args=(options.directory_name,))
+            t.start()
+            time.sleep(0.2)
+            label = str(analyzer)
+            print label
+            while analyzer.working:
+                new_label = str(analyzer)
+                if label <> new_label:
+                    d3 = datetime.datetime.now()
+                    if ndoc==1:
+                        print "\t** Time spent:", (d3-d1), "**"
+                        d4 = datetime.datetime.now()
+                    else: 
+                        print "\t** Time spent:", (d3-d4), "**"
+                        d4 = datetime.datetime.now()
+                    label=new_label
+                    print label
+                    ndoc+=1
+                time.sleep(0.2)
+            d2 = datetime.datetime.now()
+            print "\t** Time spent:", (d2-d3), "**"
+            print "Index finished in ", d2-d1
         except MySQLdb.Error, e:
             print "SQL Exception: "+e.args[1]
         except FolderException, e:
