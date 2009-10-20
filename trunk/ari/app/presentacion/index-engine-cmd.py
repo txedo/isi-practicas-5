92d89b6921c6a7e5bcd19e20e7cb816bd2767f10
txedosuke <txedosuke@516645c2-a45c-11de-b98f-4500b42dc123>
2009-10-20 22:57:35 +0000

    Preparada la aplicación para su distribución * Importado psyco en todos los ficheros * Todos los ficheros codificados con utf-8 * Rutas absolutas para importar paquetes * GUI: boton "start" desactivado mientras se trabaja * Añadida la licencia GPLv3 (license.txt) * Añadido un manual de instalación y uso (readme.txt)
    Terminado el manual de usuario de la documentacion
    * Se adjuntan 2 imagenes de la interfaz de usuario y modo texto
    
    Borro directorios y ficheros de prueba

--- trunk/ari/app/presentacion/index-engine-cmd.py
+++ trunk/ari/app/presentacion/index-engine-cmd.py
@@ -11,15 +11,32 @@
 # Keywords:
 # index engine
 
+#     This file is part of Index Engine v1.0
+#     Copyright (C) 2009 Jose Domingo López & Juan Andrada Romero
+
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import psyco
-import sys
-sys.path.append('../dominio')
+psyco.full()
+
+import sys, os
+sys.path.append(os.getcwd() + "/../dominio")
+
 import analyzer
 import MySQLdb
 import datetime
 import threading, time
-
-psyco.full()
 
 from optparse import OptionParser
 from exception import *
