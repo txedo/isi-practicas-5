27f6215b62771c29e052b7ec6d5135a244309a34
juanro.1987 <juanro.1987@516645c2-a45c-11de-b98f-4500b42dc123>
2009-11-25 20:21:39 +0000

    ARI: - Actualizado el "search-engine" para usarlo como entregable final - Añadido directorio "repository" al search-engine - Modificado dao, eliminando metodos que no se usaban y añadiendo alguno nuevo - añadido "config" al search-engine - Añadida una tabla auxiliar para acelerar la indexacion - Renombrado parser.py a parse.py (fallaba por el nombre, que es reservado de otro modulo de python) - Modificado analyzer y cache para tener en cuenta la indexacion con la nueva tabla

--- trunk/ari/index-engine/app/persistencia/dao.py
+++ trunk/ari/index-engine/app/persistencia/dao.py
@@ -19,8 +19,9 @@
 import psyco
 psyco.full()
 
-from agente import *
+import datetime
 from config import *
+from agente import *
 
 
 class Dao:
@@ -32,18 +33,57 @@ class Dao:
 
 
     def execute (self, sql):
-        self.__agente.execute(sql)
-
-
-    # Metodos para insertar terminos en las tablas de la base de datos
-
-    def insert_term_dic(self, term, frequency=1):
-        sql = "INSERT INTO dic VALUES ('"+term+"',"+str(frequency)+")"
-        try:          
+        try:
             self.__agente.execute(sql)
         except:
             raise
+
+
+    def query (self, sql):
+        try:
+            return self.__agente.query(sql)
+        except:
+            raise
+
+
+    def get_num_docs(self):
+        result = 0
+        try:
+            sql = "SELECT COUNT(id_doc) FROM doc"
+            result = int(self.query(sql)[0][0])
+        except:
+            raise
+        return result
+
+
+    def select (self, question):
+        timestamp = str(datetime.datetime.now().microsecond)
+        view_name = "view_relevant_docs_"+timestamp
+        result = None
+
+        try:
+            #d1 = datetime.datetime.now()
+            sql_view = "CREATE VIEW " + view_name + " AS SELECT DISTINCT p1.id_doc FROM posting_file p1 WHERE p1.term IN ("
+            for i in question:
+                sql_view += "'" + i + "',"
+            sql_view = sql_view[:len(sql_view)-1] + ")"
+            self.execute(sql_view)
+            #print datetime.datetime.now()-d1
+            #d1 = datetime.datetime.now()
+            sql_select = "SELECT p.term, p.id_doc, p.frequency, d.num_docs FROM posting_file p, dic d WHERE p.id_doc IN (SELECT * FROM " + view_name + ") and d.term=p.term"
+            result = self.query(sql_select)
+            #print datetime.datetime.now()-d1
+            sql_drop_view = "DROP VIEW " + view_name
+            self.execute(sql_drop_view)
+            #input()
+        except:
+            raise
         
+        #(term, id_doc, frequency, num_docs)
+        return result
+
+
+    # Metodos para insertar terminos en las tablas de la base de datos     
 
     def insert_term_dic_duplicate(self, term_buffer):
         sql = "INSERT INTO dic (term, num_docs) VALUES "
@@ -51,23 +91,27 @@ class Dao:
             for i in term_buffer:
                 sql += "('"+i+"', " + str(term_buffer[i]) + "),"
             sql = sql[:len(sql)-1]+" ON DUPLICATE KEY UPDATE num_docs=num_docs+VALUES(num_docs)"
-            self.__agente.execute(sql)
+            self.execute(sql)
         except:
             raise
 
 
-    def insert_term_posting_file(self, term, id_doc):
-        sql = "INSERT INTO posting_file (term, id_doc) VALUES ('"+term+"',"+str(id_doc)+")"
-        try:
-            self.__agente.execute(sql)
+    def insert_aux(self, term_buffer):
+        sql = "INSERT INTO aux (term, num_docs) VALUES "
+        try:          
+            for i in term_buffer:
+                sql += "('"+i+"', " + str(term_buffer[i]) + "),"
+            sql = sql[:len(sql)-1]+";"
+            self.execute(sql)
         except:
             raise
 
 
-    def insert_term_posting_file(self, term, id_doc, frequency):
-        sql = "INSERT INTO posting_file VALUES ('"+term+"',"+str(id_doc)+","+str(frequency)+")"
+    def update_dic(self):
+        sql = "INSERT INTO dic (term, num_docs) (SELECT a.term, sum(a.num_docs) FROM aux a GROUP BY a.term)"
         try:
-            self.__agente.execute(sql)
+            self.execute(sql)
+            self.clean_aux()
         except:
             raise
 
@@ -77,7 +121,7 @@ class Dao:
         try:
             for k in posting_file:
                 sql += "('"+k+"',"+str(last_id)+","+str(posting_file[k])+"),"
-            self.__agente.execute(sql[:len(sql)-1]+";")
+            self.execute(sql[:len(sql)-1]+";")
         except:
             raise
 
@@ -94,57 +138,18 @@ class Dao:
             # Actualizamos la ruta del documento
             path = REPOSITORY_PATH + str(last_id) + ".txt"
             sql = "UPDATE doc SET path='" + path + "' WHERE id_doc=" + str(last_id)
-            self.__agente.execute(sql)
+            self.execute(sql)
             return (path, last_id)
         except:
             raise
 
-        
-    # Metodos para actualizar frecuencias de los terminos en el diccionario y en el posting_file
     
-    def update_term_dic(self, term, frequency):
-        sql = "UPDATE dic SET num_docs="+str(frequency)+" WHERE term='"+term+"'"
-        try:            
-            self.__agente.execute(sql)
-        except:
-            raise 
-
-
-    def update_term_posting_file(self, term, id_doc):
-        sql = "UPDATE posting_file SET frequency=frequency+1 WHERE term='"+term+"' AND id_doc="+str(id_doc)
-        try:            
-            self.__agente.execute(sql)
-        except:
-            raise 
-
-
-    # Metodos para comporbar si ya existen terminos en el diccionario y en el posting_file
-
-    def exist_term_dic(self, term):
-        sql = "SELECT num_docs FROM dic WHERE term='"+term+"'"
+    def clean_aux (self):
+        sql = "DELETE FROM aux"
         try:
-            frequency = 0
-            exist = False
-            result = self.__agente.query(sql)
-            if result:
-                exist = True
-                frequency = result[0][0]
-            return (exist,frequency)
+            self.execute(sql)
         except:
             raise
-
-
-    def exist_term_posting_file(self, term, id_doc):
-        sql = "SELECT COUNT(*) FROM posting_file WHERE term='"+term+"' AND id_doc="+str(id_doc)
-        try:
-            result = self.__agente.query(sql)
-            existe = True
-            if result[0][0] == 0: existe = False
-            return existe
-        except:
-            raise           
-
-
 
 
     def close (self):
