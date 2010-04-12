a90a6aa12860434bc652a52af6872ee5583ca499
juanro.1987 <juanro.1987@516645c2-a45c-11de-b98f-4500b42dc123>
2010-04-12 11:02:25 +0000

    Colaboracion:   - Hecho el esqueleto de la interfaz gráfica con Jigloo

---
+++ trunk/sistemas-colaboracion/plancode/app/src/presentacion/JFPrincipal.java
@@ -0,0 +1,122 @@
+package presentacion;
+import java.awt.Dimension;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+
+import javax.swing.WindowConstants;
+import javax.swing.SwingUtilities;
+
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
+public class JFPrincipal extends javax.swing.JFrame {
+	private JPanel jPnlToolBoox;
+	private JPanel jPestañaChat;
+	private JPanel jPnlUsuarios;
+	private JPanel jPanel1;
+	private JTextArea jTxtChat;
+	private JScrollPane jScrollChat;
+	private JTextArea jTxtLog;
+	private JScrollPane jScrollLog;
+	private JPanel jPestañaLog;
+	private JTabbedPane jTabbedPane;
+
+	/**
+	* Auto-generated main method to display this JFrame
+	*/
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JFPrincipal inst = new JFPrincipal();
+				inst.setLocationRelativeTo(null);
+				inst.setVisible(true);
+			}
+		});
+	}
+	
+	public JFPrincipal() {
+		super();
+		initGUI();
+	}
+	
+	private void initGUI() {
+		try {
+			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			getContentPane().setLayout(null);
+			setMinimumSize(new Dimension(892,600));
+			this.setTitle("PlanCoDE");
+			{
+				jPnlToolBoox = new JPanel();
+				getContentPane().add(jPnlToolBoox);
+				jPnlToolBoox.setBounds(0, 31, 195, 330);
+			}
+			{
+				jTabbedPane = new JTabbedPane();
+				getContentPane().add(jTabbedPane);
+				jTabbedPane.setBounds(0, 367, 876, 168);
+				{
+					jPestañaChat = new JPanel();
+					jTabbedPane.addTab("Chat", null, jPestañaChat, null);
+					jPestañaChat.setLayout(null);
+					{
+						jScrollChat = new JScrollPane();
+						jPestañaChat.add(jScrollChat);
+						jScrollChat.setBounds(0, 7, 871, 133);
+						jScrollChat.setPreferredSize(new java.awt.Dimension(436, 23));
+						{
+							jTxtChat = new JTextArea();
+							jScrollChat.setViewportView(jTxtChat);
+							jTxtChat.setEditable(false);
+						}
+					}
+				}
+				{
+					jPestañaLog = new JPanel();
+					jTabbedPane.addTab("Log", null, jPestañaLog, null);
+					jPestañaLog.setLayout(null);
+					{
+						jScrollLog = new JScrollPane();
+						jPestañaLog.add(jScrollLog);
+						jScrollLog.setBounds(0, 6, 871, 134);
+						{
+							jTxtLog = new JTextArea();
+							jScrollLog.setViewportView(jTxtLog);
+							jTxtLog.setEditable(false);
+						}
+					}
+				}
+			}
+			{
+				jPnlUsuarios = new JPanel();
+				getContentPane().add(jPnlUsuarios);
+				jPnlUsuarios.setBounds(773, 47, 103, 320);
+				jPnlUsuarios.setSize(195, 330);
+			}
+			{
+				jPanel1 = new JPanel();
+				getContentPane().add(jPanel1);
+				jPanel1.setBounds(673, 31, 195, 324);
+			}
+			pack();
+			setSize(400, 300);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
