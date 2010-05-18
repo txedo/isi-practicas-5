2a062fcd0dd8577801ec676f44d2e8332977a381
juanro.1987 <juanro.1987@516645c2-a45c-11de-b98f-4500b42dc123>
2010-05-18 18:35:48 +0000

    MAIA:    * Terminado y funcionando el algoritmo de Kohonen

--- trunk/mya-ia/kohonen/abalone.py
+++ trunk/mya-ia/kohonen/abalone.py
@@ -3,14 +3,11 @@ import psyco
 psyco.full()
 
 class Abalone:
-    def __init__(self, sex, length, diameter, height, whole_weight, shucked_weight, viscera_weight, shell_weight):
+    def __init__(self, m, f, i, length, diameter, height, whole_weight, shucked_weight, viscera_weight, shell_weight):
         # sex es de tipo string y toma los valores "M", "F", "I" El resto de atributos son de tipo real
-        self.m = 0
-        self.f = 0
-        self.i = 0
-        if sex == "M": self.m = 1
-        elif sex == "F": self.f = 1
-        else: self.i = 1
+        self.m = m
+        self.f = f
+        self.i = i
         self.length = length
         self.diameter = diameter
         self.height = height
@@ -20,7 +17,7 @@ class Abalone:
         self.shell_weight = shell_weight
     
     def obtenerSimilitud(self, other):
-        return sqrt(pow(self.m-other.m, 2)+pow(self.f-other.f, 2)+pow(self.i-other.i, 2)+pow(self.length-other.length, 2)+pow(self.diameter-other.diameter, 2)+pow(self.height-other.height, 2)+pow(self.whole_height-other.whole_height, 2)+pow(self.shucked_weight-other.shucked_weight, 2)+pow(self.viscera_weight-other.viscera_weight, 2)+pow(self.shell_weight-other.shell_weight, 2))
+        return sqrt(pow(self.m-other.m, 2)+pow(self.f-other.f, 2)+pow(self.i-other.i, 2)+pow(self.length-other.length, 2)+pow(self.diameter-other.diameter, 2)+pow(self.height-other.height, 2)+pow(self.whole_weight-other.whole_weight, 2)+pow(self.shucked_weight-other.shucked_weight, 2)+pow(self.viscera_weight-other.viscera_weight, 2)+pow(self.shell_weight-other.shell_weight, 2))
 
     def __str__ (self):
         return str(self.m) + " " + str(self.f) + " " + str(self.i) + " " + str(self.length) + " " + str(self.diameter) + " " + str(self.height) + " " + str(self.whole_weight) + " " + str(self.shucked_weight) + " " + str(self.viscera_weight) + " " + str(self.shell_weight)
--- trunk/mya-ia/kohonen/main.py
+++ trunk/mya-ia/kohonen/main.py
@@ -13,9 +13,7 @@ if __name__=="__main__":
     fileHandler = File_Handler()
     # Inicializamos cada neurona con el vector definido en el fichero
     datosPesosIniciales = fileHandler.read_lines_file("./ficheroPesos.txt")
-    mapa.inicializarNeuronas(datosPesosIniciales)
-    # for i in mapa.neuronas:
-    #    print (str(i))     
+    mapa.inicializarNeuronas(datosPesosIniciales)    
     # Establecemos el radio inicial a DIM-1, para afectar a todo el mapa
     radio = DIM -1 
     # Establecemos el numero de iteracion
@@ -24,17 +22,20 @@ if __name__=="__main__":
     factorAprendizaje = FACTOR_APRENDIZAJE_INICIAL
     # Leemos todas las lineas del fichero de datos de los abalones y vamos reorganizando el mapa de Kohonen
     # Cada linea representa un abalone con los datos normalizados y separados los valores de los atributos por espacios
-    datosAbalone = fileHandler.read_lines_file("./ficheroDatosAbalones.txt")
+    datosAbalone = fileHandler.read_lines_file("./abalonesNormalizadosCopia.txt")
     # Evaluamos cada vector de entrada (abalone) con el mapa
     for d in datosAbalone:
         valoresAtributos = d.split()
-        abalone = Abalone(str(valoresAtributos[0]), float(valoresAtributos[1]), float(valoresAtributos[2]), float(valoresAtributos[3]), float(valoresAtributos[4]), float(valoresAtributos[5]), float(valoresAtributos[6]), float(valoresAtributos[7]))
+        abalone = Abalone(int(valoresAtributos[0]), int(valoresAtributos[1]), int(valoresAtributos[2]), float(valoresAtributos[3]), float(valoresAtributos[4]), float(valoresAtributos[5]), float(valoresAtributos[6]), float(valoresAtributos[7]), float(valoresAtributos[8]), float(valoresAtributos[9]))
         # Se evalua la similitud, obteniendo una neurona ganadora
         mapa.evaluarSimilitud(abalone)
         # Se cambian los pesos de la vecindad de la neurona ganadora
-        mapa.variarPesosNeuronas(radio, abalone, factorAprendizaje)
+        mapa.variarPesoNeuronas(radio, abalone, factorAprendizaje)
         # Se varia el radio de vecindad y el factor de aprendizaje para la siguiente iteracion
-        radio = mapa.variarRadioVecindad(iteracion)
+        radio = mapa.variarGradoVecindad(iteracion)
         factorAprendizaje = mapa.variarFactorAprendizaje(iteracion)
         # Se aumenta la iteracion
         iteracion = iteracion + 1
+
+    for i in mapa.neuronas:
+        print (str(i))
