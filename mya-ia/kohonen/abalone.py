80cac6f5a6478574e33c1a2625a9449a218bafb6
juanro.1987 <juanro.1987@516645c2-a45c-11de-b98f-4500b42dc123>
2010-05-19 15:16:57 +0000

    MAIA:    * Cambios que hemos hecho esta mañana en Alarcos

--- mya-ia/kohonen/abalone.py
+++ mya-ia/kohonen/abalone.py
@@ -2,12 +2,9 @@ from math import *
 import psyco
 psyco.full()
 
+# Quitamos el sexo para evitar mucha distancia entre vectores. Se tendra en cuenta a la hora de sacar el volumen 
 class Abalone:
-    def __init__(self, m, f, i, length, diameter, height, whole_weight, shucked_weight, viscera_weight, shell_weight):
-        # sex es de tipo string y toma los valores "M", "F", "I" El resto de atributos son de tipo real
-        self.m = m
-        self.f = f
-        self.i = i
+    def __init__(self, length, diameter, height, whole_weight, shucked_weight, viscera_weight, shell_weight):
         self.length = length
         self.diameter = diameter
         self.height = height
@@ -17,8 +14,8 @@ class Abalone:
         self.shell_weight = shell_weight
     
     def obtenerSimilitud(self, other):
-        return sqrt(pow(self.m-other.m, 2)+pow(self.f-other.f, 2)+pow(self.i-other.i, 2)+pow(self.length-other.length, 2)+pow(self.diameter-other.diameter, 2)+pow(self.height-other.height, 2)+pow(self.whole_weight-other.whole_weight, 2)+pow(self.shucked_weight-other.shucked_weight, 2)+pow(self.viscera_weight-other.viscera_weight, 2)+pow(self.shell_weight-other.shell_weight, 2))
+        return sqrt(pow(self.length-other.length, 2)+pow(self.diameter-other.diameter, 2)+pow(self.height-other.height, 2)+pow(self.whole_weight-other.whole_weight, 2)+pow(self.shucked_weight-other.shucked_weight, 2)+pow(self.viscera_weight-other.viscera_weight, 2)+pow(self.shell_weight-other.shell_weight, 2))
 
     def __str__ (self):
-        return str(self.m) + " " + str(self.f) + " " + str(self.i) + " " + str(self.length) + " " + str(self.diameter) + " " + str(self.height) + " " + str(self.whole_weight) + " " + str(self.shucked_weight) + " " + str(self.viscera_weight) + " " + str(self.shell_weight)
+        return str(self.length) + " " + str(self.diameter) + " " + str(self.height) + " " + str(self.whole_weight) + " " + str(self.shucked_weight) + " " + str(self.viscera_weight) + " " + str(self.shell_weight)
         
