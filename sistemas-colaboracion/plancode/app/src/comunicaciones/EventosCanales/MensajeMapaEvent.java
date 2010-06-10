65eae342fb62873247bee126789489d03289d86a
juanro.1987 <juanro.1987@516645c2-a45c-11de-b98f-4500b42dc123>
2010-06-10 00:36:13 +0000

    Plancode:   * Empezados (y casi terminados) los diagramas de clases

--- sistemas-colaboracion/plancode/app/src/comunicaciones/EventosCanales/MensajeMapaEvent.java
+++ sistemas-colaboracion/plancode/app/src/comunicaciones/EventosCanales/MensajeMapaEvent.java
@@ -4,8 +4,6 @@ import java.util.EventObject;
 
 import javax.swing.ImageIcon;
 
-import dominio.conocimiento.InfoMapa;
-
 public class MensajeMapaEvent extends EventObject {
 
 	private static final long serialVersionUID = -8922139367513391169L;
